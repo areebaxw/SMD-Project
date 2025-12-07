7ba91bc3c0ca69ab834a107aefeb6f5fca658a8b
Mahad1090 <i230537@isb.nu.edu.pk>
2025-12-07 15:07:13 +0500

    Mahad Screens

---
+++ app/src/main/java/com/example/smd_project/EnterMarks.kt
@@ -0,0 +1,11 @@
+package com.example.smd_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EnterMarks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entermarks)
+    }
+}
